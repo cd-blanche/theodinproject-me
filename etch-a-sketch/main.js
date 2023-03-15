eeb7df27ad12231ada217dc17827afaac99151cc
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-03-14 20:02:07 -0400

    Add container resizing + container reset

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,21 +1,58 @@
-const container = document.querySelector('#container');
-
-for (let i = 0; i < (16 * 16); i++) {
-  const newSquare = document.createElement('div');
-  newSquare.classList.add('square');
-
-  container.appendChild(newSquare);
+window.onload = function() {
+  createGrid();
 };
 
-let addTrail = (e) => {
-  e.target.classList.add('square-trail');
+const updateButton = document.querySelector('#change');
+const resetButton = document.querySelector('#reset');
+
+updateButton.addEventListener('click', updateGrid);
+resetButton.addEventListener('click', resetGrid);
+
+function updateGrid() {
+  // select current container
+  const container = document.querySelector('#container');
+  // prompt for user input
+  gridSize = parseInt(prompt('How big would you like the container to be? (Note: max 50)'));
+  // Limits grid size to 100
+  if (gridSize > 50) gridSize = 50;
+  // remove current container
+  container.remove();
+  // creates new container
+  const newContainer = document.createElement('div');
+  newContainer.id = 'container';
+  resetButton.before(newContainer);
+  // creates new grid
+  createGrid(gridSize);
 };
 
-document.querySelectorAll('.square').forEach(square => {
+// Creates grid
+function createGrid(size) {
+  const container = document.querySelector('#container');
+  for (let i = 0; i < ((size || 16) ** 2); i++) {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    container.appendChild(newSquare);
+  };
+  // Sets proper grid size
+  container.style.setProperty('--grid-size', size || 16);
+  // Adds trail
+  addTrail()
+};
 
-  square.addEventListener('mouseenter', addTrail);
+function addTrail() {
+  const squares = document.querySelectorAll('.square');
 
-});
+  squares.forEach(square => {
+    square.addEventListener('mouseenter', e => {
+      e.target.classList.add('square-trail');
+    });
+  });
+}
 
+function resetGrid() {
+  const squares = document.querySelectorAll('.square');
 
-
+  squares.forEach(square => {
+    square.classList.remove('square-trail');
+  });
+}
